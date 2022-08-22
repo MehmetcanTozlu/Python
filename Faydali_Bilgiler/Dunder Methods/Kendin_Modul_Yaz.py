431e6ba5ac1c16fa1d8c6e31895200c39f9709c5
Mehmetcan Tozlu <65121799+MehmetcanTozlu@users.noreply.github.com>
2022-08-22 16:41:02 +0300

    Add files via upload

---
+++ Faydali_Bilgiler/Dunder Methods/Kendin_Modul_Yaz.py
@@ -0,0 +1,15 @@
+"""Dunder Methods - Kendimizde Bir Modul Yazmak Istersek"""
+
+# Bu Modulu Yazan Kisiler
+__author__ = 'Mehmetcan Tozlu'
+# Telif Hakkinin Sahibi
+__copyright__ = 'Copyright 2022, Tum Haklari Saklidir'
+# Modulun Yazilmasinda Emegi Gecenleri Liste Biciminde Yazmamiz Gerekli
+__credits__ = ['Mehmetcan Tozlu', 'Onuralp Ata', 'Buse Tozlu', 'İlhan Akten']
+
+# Modul Hakkinda Bilgiler
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__maintainer__ = 'Mehmetcan Tozlu'
+__email__ = 'mcantozlu@gmail.com'
+__status__ = 'Production'

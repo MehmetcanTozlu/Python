5a269a141e7f674d9d5cb8e1d10aefaa01e70a8f
Mehmetcan Tozlu <65121799+MehmetcanTozlu@users.noreply.github.com>
2022-08-22 14:45:28 +0300

    Add files via upload

---
+++ Data Science Library/Step by Step Data Preprocessing/ML_6_Feature_Scaling.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('Missing_Data_Ulke.csv')
+
+missingData = df.iloc[:, 1:4].values
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(missingData)
+missingData = imputer.fit_transform(missingData)
+
+ulke = df.iloc[:, 0:1].values
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+ulke[:, 0] = le.fit_transform(df.iloc[:, 0:1])
+
+ohe = preprocessing.OneHotEncoder()
+ulke = ohe.fit_transform(ulke).toarray()
+
+sonuc1 = pd.DataFrame(data=ulke, index=range(22), columns=['FR', 'TR', 'USA'])
+sonuc2 = pd.DataFrame(data=missingData, index=range(22), columns=['Boy', 'Kilo', 'Yas'])
+cinsiyet = df.iloc[:, 4:5].values
+sonuc3 = pd.DataFrame(data=cinsiyet, index=range(22), columns=['Cinsiyet'])
+
+s = pd.concat([sonuc1, sonuc2], axis=1)
+ss = pd.concat([s, sonuc3], axis=1)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(s, sonuc3, test_size=0.33, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
